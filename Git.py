b422e87b7154508401cf9a6b99c31941a32dd37e
Skúli Guðbrandsson <skuligu304@nemi.tskoli.is>
2017-01-25 13:36:20 +0000

    1.lidur

---
+++ Git.py
@@ -0,0 +1,7 @@
+#Skúli Guðbrandsson
+
+#Liður 1
+tala1 = int(input("Sláðu inn tölu eitt : "))
+tala2 = int(input("Sláðu inn tölu tvö : "))
+print("Samlagning talnanna er :",tala1 + tala2)
+print("Margföldun talnana er :",tala1 * tala2)
